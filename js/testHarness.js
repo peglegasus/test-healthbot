341929ec056a51974e6261d10b32be7efe178cc3
Clay Davis <cod7@cdc.gov>
2021-08-13 10:27:21 -0400

    second round of test harness changes.

--- js/testHarness.js
+++ js/testHarness.js
@@ -5,18 +5,30 @@ var taskIndex = 0;
 
 var loadTests = () => {
 
+    $.extend($.expr[':'], {
+        'containsi': function (elem, i, match, array) {
+            return (elem.textContent || elem.innerText || '').toLowerCase()
+                .indexOf((match[3] || "").toLowerCase()) >= 0;
+        }
+    });
+
     $(".pathList").empty();
 
+    var data = $("#testData").val()
+    data = data.replace(/\t/g, '');
+
     paths = [];
-    paths = $("#testData").val().split("\n")
+    paths = data.split("\n")
     paths = paths.filter(e => e);
 
     $(".pathCount").text("Path Count: " + paths.length)
 
     var band = true;
     paths.forEach(p => {
-        var c = band ? "light" : "dark"; band = !band;
-        $(".pathList").append($("<div class='listItem " + c + "'>" + p + "</div>"));
+        
+            var c = band ? "light" : "dark"; band = !band;
+            $(".pathList").append($("<div class='listItem " + c + "'><div class='status'></div>" + p + "</div>"));
+        
     });
 
 }
@@ -25,7 +37,7 @@ function runTests() {
 
     //$(".listItem").removeClass("selected");
     //$(".listItem:eq(0)").addClass("selected");
-
+    $(".pathList .listItem:eq("+ pathIndex +") .status").css("background-color","orange")
     parseTest();
 
 }
@@ -39,12 +51,13 @@ function parseTest() {
     var band = true;
     tasks.forEach(itm => {
         var c = band ? "light" : "dark"; band = !band;
-        $(".taskList").append($("<div class='listItem " + c + "'>" + itm + "&nbsp;</div>"));
+        $(".taskList").append($("<div class='listItem " + c + "'><div class='status'></div>" + itm + "&nbsp;</div>"));
     });
 
     window.scenario = {};
 
     loadChatBot();
+    $(".taskList .listItem:eq("+ taskIndex +") .status").css("background-color","orange")
     setTimeout(function () {
         findNext();
     }, 5000);
@@ -55,12 +68,18 @@ function findNext() {
 
     var $cbxList = $("#root").find(':checkbox:enabled');
     var $opt = $("#root").find('select:enabled option[value="' + tasks[taskIndex] + '"]');
-    var $btn = $("#root").find('button:enabled:contains("' + tasks[taskIndex] + '")');
 
-    if (tasks[taskIndex].indexOf("MSG: ") > -1) {
+    var $btn = $("#root").find('button:enabled:contains("' + tasks[taskIndex] + '")');
+    if($btn.length==0){
+        $btn = $("#root").find('button:enabled:containsi("' + tasks[taskIndex] + '")') // case insensitive
+    }
+    
+    var $input = $("#root").find('input:enabled');   
+
+    if (tasks[taskIndex].toString().indexOf("MSG: ") > -1) {
         var txt = tasks[taskIndex].split("MSG: ")[1];
         if ($("#root").find("div:contains('" + unescape(txt) + "')").length > 0) {
-            console.log("found result MSG: " + unescape(txt));
+            console.log("found result MSG: " + unescape(txt));            
         }
     }
     else if ($cbxList.length > 0
@@ -79,17 +98,27 @@ function findNext() {
         console.log("found '" + tasks[taskIndex] + "' button");
         $btn.click()
     }
+    else if ($input.length > 0) {
+        $input.val(tasks[taskIndex]);
+        $input.prop('disabled', true);
+        console.log("found input for '" + tasks[taskIndex] + "'");
+    }
     else {
-        if (taskIndex >= tasks.length-1) {
+        if (taskIndex >= tasks.length - 1) {
             taskIndex = 0;
-            tasks=[];
+            tasks = [];
+
+            $(".pathList .listItem:eq("+ pathIndex +") .status").css("background-color","green")
             pathIndex += 1;
             runTests();
             return;
         }
     }
-    taskIndex += 1;
 
+    $(".taskList .listItem:eq("+ taskIndex +") .status").css("background-color","green")
+
+    taskIndex += 1;
+    $(".taskList .listItem:eq("+ taskIndex +") .status").css("background-color","orange")
     setTimeout(function () {
         findNext();
     }, 5000);
